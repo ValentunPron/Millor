d3a2d92683553bdede8fdb3e1167dcfe2ed03d99
Valentyn <valentunpron@gmail.com>
2022-12-12 20:35:54 +0200

    :(

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Header } from './component/Header/Header';
 import { MainSlider } from './component/MainSlider/MainSlider';
+
 import './scss/app.scss'
+
+
 
 function App() {
 	return (
-		<>
-			<div className="wrapper">
-				<Header />
-				<main className="main">
-					<MainSlider />
-				</main>
-			</div>
-		</>
+		<div className="wrapper">
+			<Header />
+			<main className="main">
+				<MainSlider />
+			</main>
+		</div>
 	);
 }
 
