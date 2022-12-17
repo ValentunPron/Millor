340e39f84f7f28082d11bbbb10c90129ebf11682
Valentyn <valentunpron@gmail.com>
2022-12-17 15:10:01 +0200

    Завершення роботи над блоком скидок

--- src/component/Discount/Discount.tsx
+++ src/component/Discount/Discount.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from "swiper";
+import { Navigation } from "swiper";
 
 import styles from './Discount.module.scss';
 import "swiper/css";
@@ -11,8 +11,7 @@ import { CoffeItem } from "../CoffeItem/CoffeItem";
 
 export const Discount = (): JSX.Element => {
 
-	const buttonPrev = React.useRef(null),
-		buttonNext = React.useRef(null)
+	const swiperRef = React.useRef<any>(null);
 
 	return (
 		<div className={styles.discount}>
@@ -21,23 +20,35 @@ export const Discount = (): JSX.Element => {
 					<h2 className="title">Товары со скидкой</h2>
 					<p className={`${styles.text} text`}>Наша компания предлагает покупать товар со скидкой не только в дни распродаж <br /> или в течение действия ограниченных предложений, но и пользоваться скидками постоянно!</p>
 					<div className={styles.slider}>
-						<button ref={buttonPrev} className={`${styles.navigationButton} ${styles.buttonPrev}`} onClick={() => buttonPrev}>
+						<button className={`${styles.navigationButton} ${styles.buttonPrev}`} onClick={() => swiperRef.current.swiper.slidePrev()}>
 							<svg width="27" height="16" viewBox="0 0 27 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M0.292893 7.29289C-0.0976314 7.68341 -0.0976315 8.31658 0.292892 8.7071L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34314C8.46159 1.95262 8.46159 1.31945 8.07107 0.928929C7.68055 0.538404 7.04738 0.538404 6.65686 0.928929L0.292893 7.29289ZM27 7L1 7L1 9L27 9L27 7Z" fill="white" />
 							</svg>
 						</button>
 						<Swiper
 							modules={[Navigation]}
-							navigation={{
-								prevEl: buttonPrev.current,
-								nextEl: buttonNext.current,
-							}}
 							slidesPerView={3}
 							slidesPerGroup={1}
 							spaceBetween={21}
 							loop={true}
 							loopFillGroupWithBlank={true}
 							className={styles.sliderBody}
+							ref={swiperRef}
+							breakpoints={{
+								1: {
+									slidesPerView: 1.5,
+								},
+								380: {
+									slidesPerView: 2,
+								},
+								770: {
+									slidesPerView: 3,
+								},
+								1000: {
+									slidesPerView: 3,
+									spaceBetween: 21,
+								}
+							}}
 						>
 							<SwiperSlide>
 								<CoffeItem />
@@ -52,12 +63,13 @@ export const Discount = (): JSX.Element => {
 								<CoffeItem />
 							</SwiperSlide>
 						</Swiper>
-						<button ref={buttonNext} className={`${styles.navigationButton} ${styles.buttonNext}`}>
+						<button className={`${styles.navigationButton} ${styles.buttonNext}`} onClick={() => swiperRef.current.swiper.slideNext()}>
 							<svg width="27" height="16" viewBox="0 0 27 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M26.7071 8.70711C27.0976 8.31658 27.0976 7.68342 26.7071 7.2929L20.3431 0.928934C19.9526 0.53841 19.3195 0.538409 18.9289 0.928934C18.5384 1.31946 18.5384 1.95262 18.9289 2.34315L24.5858 8L18.9289 13.6569C18.5384 14.0474 18.5384 14.6805 18.9289 15.0711C19.3195 15.4616 19.9526 15.4616 20.3431 15.0711L26.7071 8.70711ZM-8.74228e-08 9L26 9L26 7L8.74228e-08 7L-8.74228e-08 9Z" fill="white" />
 							</svg>
 						</button>
 					</div>
+					<a href="#s" className={styles.viewAll}>Смотреть все</a>
 				</div>
 			</div>
 		</div >
