3461432f9c0bcd91fe8a098d28ad887fa7805f67
Valentyn <valentunpron@gmail.com>
2022-12-29 11:57:05 +0200

    Категорії завершено

--- src/component/CategoryButton/CategoryButton.props.ts
+++ src/component/CategoryButton/CategoryButton.props.ts
@@ -1,5 +1,6 @@
 import { DetailedHTMLProps, HtmlHTMLAttributes } from 'react';
 
 export interface CategoryButtonProps extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-
+	name: string,
+	children: any,
 }
