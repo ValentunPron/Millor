e523b6c4e6246bd97905e3ebd582cf4fbb39b468
Valentyn <valentunpron@gmail.com>
2023-01-27 22:27:04 +0200

    Добавлення в здорове харчування фільтру і товарів

--- src/pages/Eating/Eating.tsx
+++ src/pages/Eating/Eating.tsx
@@ -6,7 +6,7 @@ import mainImage from '../../assets/image/MainCatalog/04.png'
 import filterImage01 from '../../assets/image/Eating/01.png';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchTea, setLoaded } from '../../redux/action/tea';
+import { fetchEating, setLoaded } from '../../redux/action/eating';
 import { setSortBy, setSortRadio } from '../../redux/action/filter';
 import { Loading } from '../../component/CatalogItems/Loading';
 
@@ -21,22 +21,22 @@ interface filterDataInterface {
 }
 
 const filterData: filterDataInterface[] = [
-	{ type: "type", name: 'Цикорий', image: filterImage01 },
+	{ type: "type", name: 'Цикорий', image: 'https://i.ibb.co/Hht73HD/czikorij-png-big.png' },
 	{ type: "type", name: 'Ячменные напитки', image: filterImage01 },
 	{ type: 'type', name: 'none', image: 'none' },
-	{ type: "type", name: 'Напитки для здоровья', image: filterImage01 },
-	{ type: "type", name: 'Протеиновые смеси', image: filterImage01 },
-	{ type: "type", name: 'Толокняные каши', image: filterImage01 },
+	{ type: "type", name: 'Напитки для здоровья', image: 'https://i.ibb.co/mz8Bwcj/kisspng-green-tea-herbal-tea-drink-tea-png-pic-5a78f07ab42792-6905799815178753227379.png' },
+	{ type: "type", name: 'Протеиновые смеси', image: 'https://i.ibb.co/wLzh9fJ/output-onlinepngtools.jpg' },
+	{ type: "type", name: 'Толокняные каши', image: 'https://i.ibb.co/jHxRFGC/zdorovoe-pitanie-1.png' },
 ]
 
 export const Eating = ({ namePages }: teaInterface): JSX.Element => {
 	const dispatch: Function = useDispatch();
 	const [count, setCount] = React.useState(12);
 
-	const { tea, isLoaded, sortBy, sortRadio } = useSelector(({ filter, tea }: any) => {
+	const { eating, isLoaded, sortBy, sortRadio } = useSelector(({ filter, eating }: any) => {
 		return {
-			tea: tea.items,
-			isLoaded: tea.isLoaded,
+			eating: eating.items,
+			isLoaded: eating.isLoaded,
 			sortBy: filter.sortBy,
 			sortRadio: filter.sortRadio,
 		}
@@ -45,7 +45,7 @@ export const Eating = ({ namePages }: teaInterface): JSX.Element => {
 	React.useEffect(() => {
 		dispatch(setLoaded(false))
 		setTimeout(() => {
-			dispatch(fetchTea(sortBy, sortRadio));
+			dispatch(fetchEating(sortBy, sortRadio));
 		}, 200);
 	}, [sortBy, sortRadio]);
 
@@ -70,15 +70,15 @@ export const Eating = ({ namePages }: teaInterface): JSX.Element => {
 							<Link className='catalogLink' to="/tea">{namePages}</Link>
 						</div>
 						<h1 className={`titlePages`}>{namePages}</h1>
-						<img className='catalogImage eatingCatalogImage' src={mainImage} alt="eating" width={510} height={360} />
+						<img className='catalogImage eatingCatalogImage' src={mainImage} alt="eating" />
 						<div className='catalogFilter eatingCatalogFilter'>
 							{
-								filterData.map((filterTea: any, index) => (
+								filterData.map((filterEating: any, index) => (
 									index === 2
-										? <div key={`${index}_${filterTea.name}`} className='gridUpdate'></div>
-										: <button key={`${index}_${filterTea.name}`} className='filterItem' onClick={() => selectSortRadio(filterTea)}>
-											<img className='filterImage' src={filterTea.image} alt={filterTea.name} />
-											<h3 className='filterName'>{filterTea.name}</h3>
+										? <div key={`${index}_${filterEating.name}`} className='gridUpdate'></div>
+										: <button key={`${index}_${filterEating.name}`} className='filterItem' onClick={() => selectSortRadio(filterEating)}>
+											<img className='filterImage' src={filterEating.image} alt={filterEating.name} width={300} height={200} />
+											<h3 className='filterName'>{filterEating.name}</h3>
 										</button>
 								))
 							}
@@ -95,8 +95,8 @@ export const Eating = ({ namePages }: teaInterface): JSX.Element => {
 							{
 								isLoaded
 									?
-									tea.length > 0 ?
-										tea.map((coffeItem: any, index: number) => {
+									eating.length > 0 ?
+										eating.map((coffeItem: any, index: number) => {
 											if (index < count) {
 												return <TeaItem key={coffeItem.id} {...coffeItem} />
 											} else {
@@ -109,7 +109,7 @@ export const Eating = ({ namePages }: teaInterface): JSX.Element => {
 							}
 						</div>
 						{
-							tea.length > count ? <button className='viewNext' onClick={() => setCount(count + 4)}>Показать еще</button> : ''
+							eating.length > count ? <button className='viewNext' onClick={() => setCount(count + 4)}>Показать еще</button> : ''
 						}
 					</div>
 				</div>
--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -3,8 +3,9 @@ import { combineReducers } from "redux";
 import { coffe } from './coffe';
 import { tea } from './tea';
 import { vending } from './vending';
+import { eating } from "./eating";
 import { filter } from './filter';
 
 export const rootReducers = combineReducers({
-	coffe, tea, vending, filter,
+	coffe, tea, vending, eating, filter,
 })
