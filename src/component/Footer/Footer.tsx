7b4524474555b0a816c1803df8c668990c0e71f0
Valentyn <valentunpron@gmail.com>
2023-03-06 15:24:25 +0200

    final

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -10,6 +10,7 @@ import { HashLink } from "react-router-hash-link";
 export const Footer = ({ bgInfo = '' }: any): JSX.Element => {
 
 	const [toSend, setToSend] = React.useState({ from_email: '', });
+	const [activeEmail, setActiveEmail] = React.useState(false);
 
 	function onSubmit(e: any) {
 		e.preventDefault();
@@ -20,14 +21,15 @@ export const Footer = ({ bgInfo = '' }: any): JSX.Element => {
 
 		emailJs.send('service_0tbqnna', 'template_qjq46pi', templateParams, 'ovMXZVwdfqTP4zKxm')
 			.then((result: any) => {
+				setActiveEmail(true);
 				console.log(result.text);
 			}, (error: any) => {
+				alert('Електронна почта не правильна')
 				console.log(error)
 			});
 	};
 
 	const handleChange = (e: any) => {
-		console.log(e.target.value);
 		setToSend({ ...toSend, [e.target.name]: e.target.value });
 	};
 
@@ -40,14 +42,29 @@ export const Footer = ({ bgInfo = '' }: any): JSX.Element => {
 							<h2 className="title">Підписка на новини та розсилку</h2>
 							<p className="text">Підпишіться на нашу розсилку прямо зараз і будьте в курсі нових поставок, знижок та ексклюзивних пропозицій.</p>
 							<form className={styles.mailingForm}>
-								<input
-									type="email"
-									name='from_email'
-									value={toSend.from_email}
-									placeholder="Ваш email"
-									onChange={handleChange}
-									className={styles.mailingInput} />
-								<button onClick={onSubmit} className={styles.mailingButton}>Підписатися</button>
+								{
+									activeEmail
+										? <>
+											<input
+												type="email"
+												name='from_email'
+												value={toSend.from_email}
+												placeholder="Ваш email"
+												onChange={handleChange}
+												className={`${styles.mailingInput} inputButton`} />
+											<button className={`${styles.mailingButton} ${styles.active}`} disabled>Спасибо за подписку!</button>
+										</>
+										: <>
+											<input
+												type="email"
+												name='from_email'
+												value={toSend.from_email}
+												placeholder="Ваш email"
+												onChange={handleChange}
+												className={`${styles.mailingInput} inputButton`} />
+											<button onClick={onSubmit} className={styles.mailingButton}>Підписатися</button>
+										</>
+								}
 								<span className={styles.mailingRules}>Натискаючи на кнопку "Підписатися", ви приймаєте правила <a href="#s">користувальницької угоди</a></span>
 							</form>
 						</div>
